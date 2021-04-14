126a49526c96073c87e920a97bac434fdc239b93
Rachael Davis <80214032+themetalsaint@users.noreply.github.com>
2021-04-14 17:36:40 -0400

    added vars and steps

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,14 @@
+//To Do
+//Need the elements
+//Need the button to work
+//Need it to prompt if we want each character
+//Need to accept which ones user wants
+//Need to produce the correct password
+//Need a maximum length of min 8 and max 128 characters
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -9,6 +20,10 @@ function writePassword() {
   passwordText.value = password;
 
 }
+var special = [ '!', '@', '#', "$", '*', '?',]
+var num = ['1', '2', '3','4', '5', '6', '7', '8', '9', '0']
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
